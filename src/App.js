d41f2c31e0d4089df57f237fa80cfd7544c43f98
Bob Vawter <bobvawter@msn.com>
2021-06-10 12:09:34 -0400

    task component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
     const name = 'Bob'
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'July 9 at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Hair cut',
+            day: 'June 18 at 1:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Groceries',
+            day: 'June 10 at 8:00am',
+            reminder: false,
+        }
+    ])
+
+    // Delete Task
+    const deleteTask = (id) => {
+        console.log('delete', id)
+        setTasks(tasks.filter((task) => task.id !== id))
+    }
 
     return (
         <div className="container">
             <Header title="Header"/>
-            <h1>Hello From React</h1>
             <h2>Hello {name}</h2>
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />:'No Tasks'}
         </div>
     );
 }
