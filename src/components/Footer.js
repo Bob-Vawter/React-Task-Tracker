964983599bf8b9d83d436cbaecdcd08b5f58bca3
Bob Vawter <bobvawter@msn.com>
2021-06-16 20:42:27 -0400

    added btns to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaTwitter } from 'react-icons/fa'
-import { FaEnvelope} from 'react-icons/fa'
+import { FaEnvelope } from 'react-icons/fa'
+import Button from './Button'
 
-const Footer = () => {
+const Footer = (onClear, onDefault) => {
     return (
         <div className='footer'>
+            <div className='footer-btn'>
+                <Button
+                    color='red'
+                    text='Clear'
+                    onClick={onClear} />
+                <Button
+                    color='blue'
+                    text='Default'
+                    onClick={onDefault} />
+            </div>
             <a href='https://bobvawter.netlify.app/'>Portfolio</a>
             <ul className="icons">
                 <li><a href="https://www.linkedin.com/in/vawterbob/"><FaLinkedin /></a></li>
